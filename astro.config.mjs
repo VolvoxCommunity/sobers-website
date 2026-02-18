c4818ee20713049d2b1fdc5418126c761e56bfe8
kaushiksaravanan <75175772+kaushiksaravanan@users.noreply.github.com>
2026-02-18 19:48:45 +0000

    Reconfigure for Render Deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,10 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 import spotlight from "@spotlightjs/astro";
-import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   site: "https://www.swapgifts.store",
   output: "static",
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
   vite: {
     plugins: [tailwindcss()],
   },
